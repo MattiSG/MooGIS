7cb3d21fc1635cb71f5fbcd12a778e96cb8ad7ec
Matti Schneider-Ghibaudo <alcmene.gs@gmail.com>
2011-09-09 21:58:33 +0200

    First take at Filter's implementation.

---
+++ Source/MooGIS.Filter.GeoJSON.js
@@ -0,0 +1,62 @@
+/*
+---
+description: Defines a MooGIS filter over a GeoJSON stream.
+
+license: to be determined
+
+authors:
+- Matti Schneider-Ghibaudo
+
+requires:
+- core/1.3: '*'
+- MooGIS.Source
+
+provides: [MooGIS.Source.GeoJSON]
+
+version: 0.0.1
+
+---
+*/
+
+/**See documentation in Docs/Filters.md
+*
+*/
+MooGIS.Filter.GeoJSON = new Class({
+	Extends: MooGIS.Filter,
+	
+	style: { },
+	
+	initialize: function init(source, options) {
+		this.parent(source, options);
+	},
+	
+	/**Sets this filter's output stream's style and fires a `set` event for updating.
+	*TODO:	create a `style` event rather than re-filtering everything.
+	*
+	*@param	Object	style	a Hash of the following form, where all keys are optional:
+	*	{
+	*		className:	"a-css-class-name",
+	*		color:		"any CSS color",
+	*		opacity:	integer between 0 and 1,
+	*		size:		,
+	*		icon:		"url/to/image",
+	*		shadow:		"url/to/image"
+	*	}
+	*/
+	setStyle: function setStyle(style) {
+		this.style = style;
+		this.reload();
+	},
+	
+	/**Applies this Filter's current style to the given geojson data.
+	*Of course, styling GeoJSON-encoded data doesn't make any sense. It is simply a way to specify how the GeoJSON stream should be rendered by the map view. It is finally its responsibility to render it properly.
+	*Overwrites previous style for the keys given in the `style` param, leaves the others alone.
+	*
+	*@returns	the stream, with style information
+	*@param	GeoJSON	geojson	the data on which to apply this style
+	*/
+	_paintData: function _paintData(geojson) {
+		geojson.style = Object.merge(geojson.style, style);
+		return geojson;	
+	}
+});
---
+++ Source/MooGIS.Filter.js
@@ -0,0 +1,94 @@
+/*
+---
+description: Defines a MooGIS filter.
+
+license: to be determined
+
+authors:
+- Matti Schneider-Ghibaudo
+
+requires:
+- core/1.3: '*'
+- MooGIS.Source
+
+provides: [MooGIS.Filter]
+
+version: 0.0.1
+
+---
+*/
+
+/**See documentation in Docs/Filters.md
+*
+*/
+MooGIS.Filter = new Class({
+	Extends: MooGIS.Source,
+	
+	/**The input stream, a Source-compliant object.
+	*/
+	_source: null,
+	
+	options: {
+		/**Use a cache or not? This is a memory/speed tradeoff, and most of the time not worth it since events are used. Double-check that your code really needs it.
+		* NOT IMPLEMENTED (to be thought about: really clutters the code here. Think about an event-based Mixin).
+		*/
+//		cache: false
+	},
+	
+	initialize: function init(source, options) {
+		this.parent(options);
+		this.setSource(source);		
+	},
+	
+	/**Sets this Filter's source and fires a `set` event.
+	*Set to null if you want to remove the source (in which case the filter has an empty stream).
+	*/
+	setSource: function setSource(source) {
+		this._source = source;
+		if (source) {
+			['set', 'add', 'remove'].each(function(event) {
+				source.addEvent(eventName, this['_handle' + eventName.capitalize()].bind(this))
+			});
+			
+			this.reload();
+		}
+		
+		return this;
+	},
+	
+	_handleSet: function _handleSet(features) {
+		features = features.filter(this.accepts, this)
+		
+		this.fireEvent('set', features);
+	},
+	
+	_handleAdd: function _handleAdd(features) {
+		features = features.filter(this.accepts, this);
+		
+		this.fireEvent('add', features);
+	},
+	
+	_handleRemove: function _handleRemove(features) {
+		this.fireEvent('remove', features);
+	},
+	
+	stream: function stream() {
+		if (! this.source)
+			return [];
+		
+		return this._source.stream().filter(this.accepts, this);
+	},
+	
+	complement: function complement() {
+		if (! this.source)
+			return [];
+	
+		return this._source.stream().filter(function(feature) {
+			return ! this.accepts(feature);
+		}, this);
+	}
+	
+	/*To be implemented:
+	*	accepts(feature)	(returns bool)
+	*/
+});
