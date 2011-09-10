c07e689c3ba8cf14019f780a8cb8aeafb29878e1
Matti Schneider-Ghibaudo <alcmene.gs@gmail.com>
2011-09-10 10:49:31 +0200

    Passing tests! Added tests.

--- Tests/unit/MooGIS.Filter.test.js
+++ Tests/unit/MooGIS.Filter.test.js
@@ -18,6 +18,11 @@ describe('Filter', {
 	},
 	
 	before: function() {
+		if (subject)
+			subject.setSource(); // very important: otherwise, the source still has a reference to the previous subject, which is not useful anymore but still has a closure reference to the `signal`
+		if (secondSubject)
+			secondSubject.setSource();
+		
 		signal = secondSignal = 0;
 	
 		subject = new MooGIS.Filter.PassThrough(source);
@@ -37,15 +42,28 @@ describe('Filter', {
 	
 	"Source 'set' events are forwarded, and forwarded once": function() {
 		source.reload();
-		
 		value_of(signal).should_be(1);
+		
+		source.reload();
+		value_of(signal).should_be(2);
 	},
 	
 	"Chained 'set' events are forwarded, and forwarded once": function() {
 		source.reload();
-		
 		value_of(secondSignal).should_be(1);
-	}
+		
+		source.reload();
+		value_of(secondSignal).should_be(2);
+	},
+	
+	"Changing the source removes all previous event bindings": function() {
+		source.reload();
+		subject.setSource(null);
+		source.reload();
+		
+		value_of(signal).should_be(1);
+		value_of(secondSignal).should_be(1);
+	},
 });
 
 
