8b9f3de26aceff6921009a28e32b2c55ba7db75a
Matti Schneider-Ghibaudo <alcmene.gs@gmail.com>
2011-09-10 12:26:59 +0200

    Added Source sugar.

--- Source/MooGIS.sugar.js
+++ Source/MooGIS.sugar.js
@@ -29,7 +29,7 @@ var gis = MooGIS;
 *@returns	the new class. Note: the fully namespaced filter will also be created and made available globally.	
 *@param	channel	String	the channel to which this filter may be applied. The corresponding MooGIS.Filter.<Channel> class must exist.
 *@param	name 	String	the Filter's name (`<Implementation>`)
-*@param	accepts	Function	a `Filter.accepts` method
+*@param	accepts	Function	a `Filter.accepts(feature)` method
 */
 gis.filter = function newFilter(channel, name, accepts) {
 	return MooGIS.Filter[channel][name.capitalize()] = new Class({
@@ -37,4 +37,25 @@ gis.filter = function newFilter(channel, name, accepts) {
 		
 		accepts: accepts
 	});
+}
+
+/**Defines a `MooGIS.Source.<Channel>.<Implementation>` class.
+*
+*@returns	the new class. Note: the fully namespaced source will also be created and made available globally.	
+*@param	channel	String	the channel that this source may feed. The corresponding MooGIS.Source.<Channel> class must exist.
+*@param	name 	String	the Source's name (`<Implementation>`)
+*@param	Stream	Function	a `Source.stream()` method
+*@param	optional	helpers	Object	methods that will be implemented in the new class. Same syntax as for `Class.implement` params.
+*/
+gis.source = function newSource(channel, name, stream, helpers) {
+	var result = MooGIS.Source[channel][name.capitalize()] = new Class({
+		Extends: MooGIS.Source[channel],
+		
+		stream: stream
+	});
+	
+	if (helpers)
+		result.implement(helpers);
+	
+	return result;
 }
